be2a77ad4923332bc42f286e8d908d0126d09eef
Zach Lang <zach.d.lang@gmail.com>
2026-05-06 00:38:25 +1200

    Adds Sentry integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
+import * as Sentry from "@sentry/react-native";
 import { Stack } from "expo-router";
 
-export default function Layout() {
+Sentry.init({
+  dsn: "https://74e37cfea8703595ffd65f353854184b@o4509541345591296.ingest.de.sentry.io/4511337311436881",
+  sendDefaultPii: true,
+  enableLogs: true,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+});
+
+export default Sentry.wrap(function Layout() {
   return <Stack screenOptions={{ headerShown: false }} />;
-}
+});
