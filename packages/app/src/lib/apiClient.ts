c0b098db12d696f8d25e9415bae884458a10cd67
Zach Lang <zach.d.lang@gmail.com>
2026-05-11 21:22:32 +1200

    [WIP] Removing RPC generation

--- packages/app/src/lib/apiClient.ts
+++ packages/app/src/lib/apiClient.ts
@@ -1,4 +1,5 @@
 import type { AppType } from "@loyalty-hive/api-rpc-spec";
 import { hc } from "hono/client";
 
+// FIXME: Deleting the node_modules dir fixes this, wtf is going on here?
 export const apiClient = hc<AppType>(`${import.meta.env.API_URL}/api`);
