dc4c82705217f918c2f930937c025cc85b68ac84
Zach Lang <zach.d.lang@gmail.com>
2026-05-23 13:18:55 +1200

    Updates brand logos to use cropped versions

---
+++ .changeset/tricky-mirrors-doubt.md
@@ -0,0 +1,5 @@
+---
+"@loyalty-hive/api": patch
+---
+
+Updated all brand logos to use cropped versions for more consistent sizing.
---
+++ packages/api/drizzle/0014_charming_toad.sql
@@ -0,0 +1,16 @@
+-- Update brand logos to new cropped versions
+
+UPDATE brands SET logo_file = 'aa-v2.png' WHERE name = 'AA';
+UPDATE brands SET logo_file = 'city-chic-v2.png' WHERE name = 'City Chic';
+UPDATE brands SET logo_file = 'club-plus-v2.png' WHERE name = 'Club+';
+UPDATE brands SET logo_file = 'everyday-rewards-v2.png' WHERE name = 'Everyday Rewards';
+UPDATE brands SET logo_file = 'farmers-v2.png' WHERE name = 'Farmers';
+UPDATE brands SET logo_file = 'hoyts-v2.png' WHERE name = 'Hoyts';
+UPDATE brands SET logo_file = 'living-rewards-v2.png', background_color = '#FFFFFF' WHERE name = 'Living Rewards';
+UPDATE brands SET logo_file = 'mecca-v2.png' WHERE name = 'Mecca';
+UPDATE brands SET logo_file = 'muffin-break-v2.png' WHERE name = 'Muffin Break';
+UPDATE brands SET logo_file = 'pak-n-save-v2.png' WHERE name = 'PAK''nSAVE';
+UPDATE brands SET logo_file = 'strandbags-v2.png' WHERE name = 'Strandbags';
+UPDATE brands SET logo_file = 'subway-v2.png', background_color = '#009744' WHERE name = 'Subway';
+UPDATE brands SET logo_file = 'supercheap-auto-v2.png' WHERE name = 'Supercheap Auto';
+UPDATE brands SET logo_file = 'the-athletes-foot-v2.png', background_color = '#FFFFFF' WHERE name = 'The Athlete''s Foot';
